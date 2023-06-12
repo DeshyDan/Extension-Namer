badbb1846e0edbc02587d9581101e0cb3c318c9c
Deshy Dan <deshy733@gmail.com>
2023-06-12 13:26:59 +0200

    Updated setup configurations

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-import setuptools
+from setuptools import setuptools, find_packages
 from pathlib import Path
 
 setuptools.setup(
     name="extensionNamer",
     version="1.0.0",
+    author="Deshy Dan",
     long_description=Path("README.md").read_text(),
-    pakages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={"extensionNamer": ["extensionNamer/extensionNames.json"]}
+
 )
